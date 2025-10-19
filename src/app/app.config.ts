d84c59f7c603cdeb87b921d50e3eb03aa53cb48f
georgyguei <georgy.guei@efrei.net>
2025-10-19 03:12:15 +0200

    feat(auth): add registration form and first http POST

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
+import { provideHttpClient, withFetch } from "@angular/common/http";
 import { type ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient(withFetch()), // Enable HttpClient with fetch API
+  ],
 };
