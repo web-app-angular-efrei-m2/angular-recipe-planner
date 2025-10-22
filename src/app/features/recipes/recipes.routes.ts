ba9c8e873ddf73bb8f43aa465c514f8c8340a795
georgyguei <georgy.guei@efrei.net>
2025-10-22 10:08:55 +0200

    feat: Implement Recipe Review and Reviews List Components

--- src/app/features/recipes/recipes.routes.ts
+++ src/app/features/recipes/recipes.routes.ts
@@ -1,4 +1,5 @@
 import type { Routes } from "@angular/router";
+import { authGuard } from "@/app/core/guards/auth-guard";
 
 export const RECIPE_ROUTES: Routes = [
   {
@@ -8,5 +9,14 @@ export const RECIPE_ROUTES: Routes = [
   {
     path: ":id",
     loadComponent: () => import("./recipe-detail/recipe-detail.component").then((m) => m.RecipeDetailComponent),
+  },
+  {
+    path: ":id/reviews",
+    loadComponent: () => import("./recipe-reviews-list/recipe-reviews-list.component").then((m) => m.RecipeReviewsListComponent),
+  },
+  {
+    path: ":id/review/new",
+    loadComponent: () => import("./recipe-review/recipe-review.component").then((m) => m.RecipeReviewComponent),
+    canActivate: [authGuard],
   },
 ];
